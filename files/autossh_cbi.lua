2a60648af433e4fe5b4d453113ba9bdd7620cda1
LoveSy <631499712@qq.com>
2017-09-22 21:16:22 +0800

    Add features

--- files/autossh_cbi.lua
+++ files/autossh_cbi.lua
@@ -16,13 +16,23 @@ o = s:option(Flag, "enabled", translate("Enable"))
 o.default     = o.enabled
 o.rmempty     = false
 
+o = s:option(Value, "localhost", translate("本地主机"))
+o.placeholder = "localhost"
+o.datatype    = "ipaddr"
+o.rmempty     = true
+
 o = s:option(Value, "localport", translate("本地端口"))
 o.placeholder = 22
 o.datatype    = "port"
 o.rmempty     = false
 
+o = s:option(Value, "remotehost", translate("远程主机"))
+o.placeholder = "0.0.0.0"
+o.datatype    = "ipaddr"
+o.rmempty     = true
+
 o = s:option(Value, "remoteport", translate("远程端口"))
-o.placeholder = 19999
+o.placeholder = 22222
 o.datatype    = "port"
 o.rmempty     = false
 
@@ -30,12 +40,27 @@ o = s:option(Value, "monitorport", translate("监视端口"))
 o.datatype    = "port"
 o.rmempty     = false
 
-o = s:option(Value, "ssh", translate("远程登陆"))
+o = s:option(Value, "sshhost", translate("ssh主机"))
 o.rmempty     = false
+
+o = s:option(Value, "sshuser", translate("ssh用户"))
+o.placeholder = 'root'
+o.datatype    = "string"
+o.rmempty     = true
+
+o = s:option(Value, "sshport", translate("ssh端口"))
+o.placeholder = 22
+o.datatype    = "port"
+o.rmempty     = true
+
+o = s:option(Value, "sshidentify", translate("ssh密钥"))
+o.placeholder = "/etc/autossh/id_rsa"
+o.datatype    = "file"
+o.rmempty     = true
 
 o = s:option(Value, "poll", translate("轮询时间"))
 o.placeholder = 600
-o.default     = 600
 o.datatype    = "uinteger"
+o.rmempty     = true
 
 return m
